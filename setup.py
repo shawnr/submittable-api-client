ac1dac42cc94eaf8ce9bd031daf4494045e23a7f
Shawn Rider <shawn@shawnrider.com>
2014-06-16 12:04:10 -0700

    Version bump.
    Change-Id: I2ed03ec0e62b89c7c21ca6c12c327b8b8a59798e

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
     name='submittable_api_client',
     packages=['submittable_api_client'],
-    version='0.3',
+    version='0.4',
     license='MIT',
     description='A client wrapper for the Submittable.com API.',
     author='Shawn Rider',
     author_email='shawn@shawnrider.com',
     url='https://github.com/shawnr/submittable-api-client',
     download_url="""
-        https://github.com/shawnr/submittable-api-client/archive/0.3.zip""",
+        https://github.com/shawnr/submittable-api-client/archive/0.4.zip""",
     keywords=['API', 'REST', 'Submittable'],
     classifiers=[],
 )
